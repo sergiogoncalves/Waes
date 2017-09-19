e45be8e8fb157769e473f1aa766ddd9b7a5f00e6
Sergio Goncalves <eu.sergiogoncalves@gmail.com>
2017-09-19 18:43:11 -0400

    Improve the unit tests

--- src/test/java/com/wearewaes/controller/mapper/PersonMapperTest.java
+++ src/test/java/com/wearewaes/controller/mapper/PersonMapperTest.java
@@ -2,14 +2,19 @@ package com.wearewaes.controller.mapper;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import com.wearewaes.datatransferobject.PersonDTO;
 import com.wearewaes.domainvalue.Messages;
 import com.wearewaes.exception.NotPersonDTOException;
 
 public class PersonMapperTest {
-
+	
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+	
 	@Test
 	public void shouldReturnAPersonDTOObjectByEncodedString() throws NotPersonDTOException {
 
@@ -22,6 +27,15 @@ public class PersonMapperTest {
 
 		assertFalse(PersonMapper.makePersonDTODecoded(Messages.DECODED_NOT_PERSON_DO_STRING) instanceof PersonDTO);
 
+	}
+	
+	@Test
+	public void shouldNotReturnAPersonDTOObjectByEncodedString_2() throws NotPersonDTOException {
+
+		exception.expect(NotPersonDTOException.class);
+		
+		PersonMapper.makePersonDTODecoded(Messages.DECODED_NOT_PERSON_DO_STRING);
+		
 	}
 
 }
